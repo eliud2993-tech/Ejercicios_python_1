d2f5847a084f47051cb52e6c866a6ed342036aa0
Eliud Flores <eliud2993@gmail.com>
2026-01-23 02:35:41 -0600

    mi segundo commit

---
+++ ejercicio_05.py
@@ -0,0 +1,8 @@
+# convertir grados Fahrenheit a grados Celsius
+
+fahrenheit = float(input('ingresa los grados fahrenheit'))
+
+celsius = (fahrenheit - 32) * (5 / 9)
+
+print('fahrenheit', fahrenheit)
+print('celsius', celsius)
---
+++ tipos de datos.py
@@ -0,0 +1,14 @@
+tipos de datos
+
+
+
+name = "Eliud"
+age = 19
+height = 1.64
+is_singler = true
+
+print(name)
+print(age)
+print(height)
+print(is_singler)
+
