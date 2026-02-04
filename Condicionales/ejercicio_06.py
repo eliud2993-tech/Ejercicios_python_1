82eabe2ec32d6ca998821a5989c5fb07a36cc28d
Eliud Flores <eliud2993@gmail.com>
2026-02-04 07:51:57 -0600

    agregando mis archivos

---
+++ Condicionales/ejercicio_06.py
@@ -0,0 +1,9 @@
+# Programa que lee una letra y dice si es Mayuscula
+
+letter = input('Ingresa una letra: ')
+
+if letter == letter.upper():
+	print('Es una letra Mayuscula!')
+else:
+	print('No es una letra Mayuscula!')
+	
